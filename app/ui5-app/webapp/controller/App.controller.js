5871177290d43b35e21cf8874b680eef620c0a8d
Fabian Orjuela <fabianormo@outlook.com>
2025-06-22 17:26:25 +0000

    feat: 🎸 Add app

---
+++ app/ui5-app/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("ormobtp.ui5app.controller.App", {
+      onInit() {
+      }
+  });
+});
